bafbd72e7ee7d4c8989064850946629d44c997a3
femi bello <joshuaobello95@gmail.com>
2026-03-03 11:20:42 -0600

    continued refactoring

--- buildtool/src/image.rs
+++ buildtool/src/image.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use crate::{Target, build_image, build_kernel};
+use buildtool::{Target, build_image, build_kernel};
 
 pub fn run(release: bool, target: Target) -> Result<()> {
     build_image(&build_kernel(release, target)?, release, target)?;
