af7988ac69207aa977bd0fdd2bcc1e8a3c9e09f4
mattyb709 <matthew.builes8@gmail.com>
2026-03-04 10:24:23 -0600

    fixed page table permission issue, added multiple contiguous frame allocator

--- src/physical_memory.rs
+++ src/physical_memory.rs
@@ -123,6 +123,32 @@ pub fn frame_alloc() -> usize {
     }
 }
 
+// maps 'frames' number of contiguous frames
+// TODO we don't really want this to be a bump allocator we want to be able
+// to better reclaim memory
+pub fn alloc_frames(frames: usize) -> usize {
+    if frames == 0 {
+        0;
+    }
+
+    let mut end = END.lock();
+    let regions = unwrap(&REGIONS);
+    while end.offset + Arch::PAGE_SIZE * frames > regions[end.region].length as usize {
+        if let Some(region) = ((end.region + 1)..regions.len())
+            .find(|&r| regions[r].entry_type == EntryType::USABLE)
+        {
+            *end = FrameLocation { region, offset: 0 };
+            continue;
+        }
+        panic!("No usable memory regions found");
+    }
+    
+    let entry = unwrap(&REGIONS)[end.region];
+    let frame: usize = entry.base as usize + end.offset;
+    end.offset += Arch::PAGE_SIZE * frames;
+    frame
+}
+
 pub fn frame_dealloc(frame: usize) {
     let mut head = HEAD.lock();
     unsafe {
