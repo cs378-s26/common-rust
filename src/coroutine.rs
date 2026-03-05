263e43eba5abc9827a6308a94e0defe63698b2bc
Jiaying Song <song.jiaying04@gmail.com>
2026-03-05 00:35:28 -0600

    test: add tests for coroutines

--- src/coroutine.rs
+++ src/coroutine.rs
@@ -155,3 +155,86 @@ pub fn init_coroutine_queue() {
 pub fn init_coroutine_executor() {
     spawn_thread(|| GLOBAL_EXECUTOR.get().unwrap().run());
 }
+
+#[cfg(test)]
+mod test {
+    use super::{spawn_coroutine};
+    use crate::{print::kprintln, thread::yield_thread};
+    use alloc::sync::Arc;
+    use core::sync::atomic::{AtomicU64, Ordering};
+    use core::future::Future;
+    use core::pin::Pin;
+    use core::task::{Context, Poll};
+
+    #[test_case]
+    fn test_coroutine_counter() {
+        const COUNT: u64 = 400;
+        let counter = Arc::new(AtomicU64::new(0));
+
+        
+        for _ in 0..COUNT {
+            let counter_clone = counter.clone();
+            spawn_coroutine(async move {
+                counter_clone.fetch_add(1, Ordering::SeqCst);
+            });
+        }
+        
+        // Wait for all coroutines to finish.
+        while counter.load(Ordering::SeqCst) < COUNT {
+            yield_thread();
+        }
+        
+        assert_eq!(counter.load(Ordering::SeqCst), COUNT);
+        kprintln!("Coroutine counter test passed: {}", COUNT);
+    }
+
+    struct IntFuture {
+        value: u64,
+        polls: u64,
+    }
+
+    impl Future for IntFuture {
+        type Output = u64;
+
+        fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            if self.polls > self.value { // Pend this many times before ready.
+                Poll::Ready(self.value + 445)
+            } else {
+                self.get_mut().polls += 1;
+                cx.waker().wake_by_ref(); // Ready to be polled again.
+                Poll::Pending
+            }
+        }
+    }
+
+    async fn async_int(number: u64) -> u64 {
+        IntFuture {
+            value: number,
+            polls: 0,
+        }
+        .await
+    }
+
+    async fn async_task(counter: Arc<AtomicU64>) {
+        for i in 0..4 {
+            let n = async_int(i).await;
+            assert_eq!(n, i + 445);
+        }
+        kprintln!("Async task {} complete.", counter.fetch_add(1, Ordering::SeqCst));
+    }
+
+    #[test_case]
+    fn test_future_polling() {
+        const TASKS: u64 = 112;
+        let counter = Arc::new(AtomicU64::new(0));
+        for _ in 0..TASKS {
+            spawn_coroutine(async_task(counter.clone()));
+        }
+        while counter.load(Ordering::SeqCst) < TASKS {
+            yield_thread();
+        }
+
+        assert_eq!(counter.load(Ordering::SeqCst), TASKS);
+        kprintln!("Coroutine future polling test passed.");
+    }
+}
