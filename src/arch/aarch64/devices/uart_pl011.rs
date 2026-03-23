2b9b9b3de4e931af58cdd7b62e75f068c068344e
mattyb709 <matthew.builes8@gmail.com>
2026-03-23 13:35:30 -0500

    created uart driver; parsed device tree; added new ArchTrait functions for arch specific device handling

--- src/arch/aarch64/devices/uart_pl011.rs
+++ src/arch/aarch64/devices/uart_pl011.rs
@@ -1,8 +1,11 @@
+use crate::arch::{Arch, ArchTrait};
 use crate::devices::device_discovery::{DeviceDiscovery, DeviceDriver, DeviceNode, DeviceType};
+use crate::print::{CharSink, kprintln};
 use crate::virtual_memory::{PagingOptions, VirtualMemoryAllocation};
 use alloc::boxed::Box;
 use fdt::node::FdtNode;
 
+// TODO currently this driver literally just writes out a character at a time, this will need to be expanded to include buffering and interrupts
 pub struct UartPl011Driver {
     phys_address: usize,
     virt_mapping: Option<VirtualMemoryAllocation>,
@@ -15,16 +18,39 @@ impl DeviceDriver for UartPl011Driver {
     }
 
     fn init(&mut self) -> bool {
-        // let options = PagingOptions::PRESENT | PagingOptions::WRITABLE;
-        // let backing = Some(self.phys_address);
-        // let vm = VirtualMemoryAllocation::new(Arch::get_address_space, Arch::PAGE_SIZE, Some(backing), options);
-        // self.virt_mapping = Some(vm);
+        let options = PagingOptions::PRESENT | PagingOptions::WRITABLE;
+        let backing = Some(self.phys_address);
+        let vm = VirtualMemoryAllocation::new(
+            Arch::get_address_space(),
+            None,
+            Arch::PAGE_SIZE,
+            backing,
+            options,
+        );
+        if let Some(mapping) = vm {
+            self.virt_mapping = Some(mapping);
+        } else {
+            return false;
+        }
         return true;
     }
 
     fn device_type(&self) -> DeviceType {
-        return DeviceType::OTHER;
+        return DeviceType::CHAR;
     }
+}
+
+impl CharSink for UartPl011Driver {
+    unsafe fn putc(&self, c: u8) {
+        if let Some(mapping) = &self.virt_mapping {
+            unsafe {
+                let base = mapping.base;
+                core::ptr::write_volatile(base as *mut u8, c);
+            }
+        }
+    }
+
+    unsafe fn flush(&self) {}
 }
 
 pub struct UartPl011Discovery;
