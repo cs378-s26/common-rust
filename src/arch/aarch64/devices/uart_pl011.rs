741f6f4ac189ad00b2060d6b517bc0817dabb165
mattyb709 <matthew.builes8@gmail.com>
2026-03-23 10:02:23 -0500

    read fdt device node

--- src/arch/aarch64/devices/uart_pl011.rs
+++ src/arch/aarch64/devices/uart_pl011.rs
@@ -1,18 +1,17 @@
-use crate::devices::device_discovery::{DeviceType, DeviceDiscovery, DeviceDriver, DeviceNode};
-use crate::virtual_memory::{VirtualMemoryAllocation, PagingOptions};
-use fdt::node::FdtNode;
+use crate::devices::device_discovery::{DeviceDiscovery, DeviceDriver, DeviceNode, DeviceType};
+use crate::virtual_memory::{PagingOptions, VirtualMemoryAllocation};
 use alloc::boxed::Box;
+use fdt::node::FdtNode;
 
 pub struct UartPl011Driver {
     phys_address: usize,
-    virt_mapping: Option<VirtualMemoryAllocation>
+    virt_mapping: Option<VirtualMemoryAllocation>,
 }
 
 impl DeviceDriver for UartPl011Driver {
-
     // defined by the driver, like uart_pl011 or virtio_blk
     fn name(&self) -> &str {
-        return "uart_pl011"
+        return "uart_pl011";
     }
 
     fn init(&mut self) -> bool {
@@ -21,31 +20,30 @@ impl DeviceDriver for UartPl011Driver {
         // let vm = VirtualMemoryAllocation::new(Arch::get_address_space, Arch::PAGE_SIZE, Some(backing), options);
         // self.virt_mapping = Some(vm);
         return true;
-
     }
 
     fn device_type(&self) -> DeviceType {
         return DeviceType::OTHER;
     }
-
 }
 
 pub struct UartPl011Discovery;
 
 impl DeviceDiscovery for UartPl011Discovery {
-    fn am_i_this(&self, node: DeviceNode<'_, '_>)  -> Option<Box<dyn DeviceDriver + Send + Sync>> {
+    fn am_i_this(&self, node: DeviceNode<'_, '_>) -> Option<Box<dyn DeviceDriver + Send + Sync>> {
         if let DeviceNode::DTB(node) = node {
             if let Some(c) = node.compatible() {
                 if c.all().any(|s| s == "arm,pl011") {
                     if let Some(reg) = node.reg().and_then(|mut r| r.next()) {
                         let phys_address = reg.starting_address as usize;
-                        return Some(Box::new(UartPl011Driver { phys_address, virt_mapping: None }));
+                        return Some(Box::new(UartPl011Driver {
+                            phys_address,
+                            virt_mapping: None,
+                        }));
                     }
                 }
             }
-            
         }
         None
     }
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -165,6 +165,8 @@ pub fn system_init<A: ArchTrait, K: KernelEntryTrait>() -> ! {
     init_physical_memory_allocator();
     init_virtual_memory_allocator();
 
+    Arch::init_arch_specific_drivers();
+
     // note we don't need to do anything special here because rust doesn't have init_array
     // if we wanted once-initialized data, we would either provide our custom mechanism,
     // or just spam OnceCell
