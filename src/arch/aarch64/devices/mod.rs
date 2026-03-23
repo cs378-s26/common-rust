c7cca964ad37b4f488e3a048d4e63569821dd8db
mattyb709 <matthew.builes8@gmail.com>
2026-03-23 13:35:30 -0500

    created uart driver; parsed device tree; added new ArchTrait functions for arch specific device handling

--- src/arch/aarch64/devices/mod.rs
+++ src/arch/aarch64/devices/mod.rs
@@ -1,1 +1,37 @@
 pub mod uart_pl011;
+use crate::devices::device_discovery::{DeviceNode, MATCHED_DEVICES, SYSTEM_DRIVERS};
+use crate::print::kprintln;
+use crate::sync::MutexLike;
+use alloc::boxed::Box;
+use fdt;
+use limine::request::DeviceTreeBlobRequest;
+
+#[used]
+#[unsafe(link_section = ".limine_requests")]
+pub static DTB_REQUEST: DeviceTreeBlobRequest = DeviceTreeBlobRequest::new();
+
+// parse the device tree and match devices to drivers. This should be called after all system drivers have been set up in SYSTEM_DRIVERS
+pub fn parse_devices() {
+    if let Some(resp) = DTB_REQUEST.get_response() {
+        let dtb_addr = resp.dtb_ptr();
+        let fdt = unsafe {
+            fdt::Fdt::from_ptr(dtb_addr as *const u8).expect("Failed to parse device tree blob.")
+        };
+        for node in fdt.all_nodes() {
+            for driver in SYSTEM_DRIVERS.lock().iter() {
+                let matched_device = driver.am_i_this(DeviceNode::DTB(node));
+                if let Some(device) = matched_device {
+                    MATCHED_DEVICES.lock().push(device);
+                }
+            }
+        }
+    }
+}
+
+pub fn create_arch_specific_drivers() {
+    // create drivers for devices that are specific to this architecture, for example aarch64's uart_pl011
+    let mut drivers = crate::devices::device_discovery::SYSTEM_DRIVERS.lock();
+    drivers.push(Box::new(uart_pl011::UartPl011Discovery));
+}
+
+pub fn init_arch_specific_drivers() {}
--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -130,6 +130,7 @@ pub trait ArchTrait {
     fn virtual_unmap(space: u64, vaddr: u64) -> Option<u64>;
     fn shutdown(err_code: u16);
     fn halt() -> !;
+    fn parse_devices(); // this must be called after all system drivers have been set up in SYSTEM_DRIVERS
     fn create_arch_specific_drivers();
     fn init_arch_specific_drivers();
 }
