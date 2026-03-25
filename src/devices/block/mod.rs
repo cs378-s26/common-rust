931367f1a1158691069012dadc9e7a71646c2215
mattyb709 <matthew.builes8@gmail.com>
2026-03-25 12:27:02 -0500

    block device interface

---
+++ src/devices/block/mod.rs
@@ -0,0 +1,24 @@
+
+pub enum BlockError {
+    InvalidBlockIndex,
+    InvalidBufferSize,
+    DeviceError,
+    // add more as needed
+}
+
+pub enum PhysicalAddressSize {
+    Size16,
+    Size32,
+    Size64,
+}
+
+pub trait BlockDevice {
+    // buffers instead of vecs to avoid needing memory allocation, but maybe this isn't necessary
+    fn init() -> Result<(), BlockError>;
+    fn read_blocks(&self, block_idxs: &[usize], buffer: &[&mut [u8]]) -> Result<(), BlockError>;
+    fn write_blocks(&self, block_idxs: &[usize], buffer: &[&[u8]]) -> Result<(), BlockError>;
+    fn flush(&self) -> Result<(), BlockError>;
+    fn block_size(&self) -> usize;
+    fn block_count(&self) -> usize;
+    fn dma_physical_address_size(&self) -> PhysicalAddressSize;
+}
