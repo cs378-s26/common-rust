d383b8f4f0096745979d985ffe2ce25d837a7039
mattyb709 <matthew.builes8@gmail.com>
2026-03-26 10:20:34 -0500

    virtio blk mapped to block device trait, not tested yet

--- src/arch/aarch64/vmm.rs
+++ src/arch/aarch64/vmm.rs
@@ -1,6 +1,6 @@
 use crate::physical_memory::{HHDM_REQUEST, frame_alloc, frame_dealloc};
-use crate::virtual_memory::PagingOptions;
 use crate::print::kprintln;
+use crate::virtual_memory::PagingOptions;
 use bitflags::bitflags;
 use core::arch::asm;
 use spin::Mutex;
