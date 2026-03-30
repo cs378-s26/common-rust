ccb14167d38fec8664948bf05675364a07fd5e59
mattyb709 <matthew.builes8@gmail.com>
2026-03-30 00:54:40 -0500

    formatted

--- src/arch/aarch64/devices/psci.rs
+++ src/arch/aarch64/devices/psci.rs
@@ -1,5 +1,4 @@
 use crate::devices::device_discovery::{DeviceDiscovery, DeviceNode, DeviceType};
-use crate::print::kprintln;
 use spin::Once;
 
 pub static PSCI_DEVICE: Once<PSCIDevice> = Once::new();
@@ -13,7 +12,7 @@ pub enum PSCIMethod {
 // given by the PSCI specification, not needed to be parsed by device discovery
 const PSCI_SYSTEM_OFF_FUNC_ID: u32 = 0x8400_0008;
 
-// below are function id's given by device parsing, as they may be different on different platforms. 
+// below are function id's given by device parsing, as they may be different on different platforms.
 // currently not used, all that is needed is shutdown
 pub struct PSCIDevice {
     _migrate: u32,
@@ -24,7 +23,13 @@ pub struct PSCIDevice {
 }
 
 impl PSCIDevice {
-    pub fn new(migrate: u32, cpu_on: u32, cpu_off: u32, cpu_suspend: u32, method: PSCIMethod) -> Self {
+    pub fn new(
+        migrate: u32,
+        cpu_on: u32,
+        cpu_off: u32,
+        cpu_suspend: u32,
+        method: PSCIMethod,
+    ) -> Self {
         Self {
             _migrate: migrate,
             _cpu_on: cpu_on,
@@ -36,8 +41,12 @@ impl PSCIDevice {
 
     pub fn shutdown(&self) {
         match self.method {
-            PSCIMethod::HVC => unsafe { core::arch::asm!("hvc #0", in("x0") PSCI_SYSTEM_OFF_FUNC_ID, options(nostack)) },
-            PSCIMethod::SMC => unsafe { core::arch::asm!("smc #0", in("x0") PSCI_SYSTEM_OFF_FUNC_ID, options(nostack)) },
+            PSCIMethod::HVC => unsafe {
+                core::arch::asm!("hvc #0", in("x0") PSCI_SYSTEM_OFF_FUNC_ID, options(nostack))
+            },
+            PSCIMethod::SMC => unsafe {
+                core::arch::asm!("smc #0", in("x0") PSCI_SYSTEM_OFF_FUNC_ID, options(nostack))
+            },
         }
     }
 }
@@ -46,14 +55,11 @@ pub struct PSCIDiscovery;
 
 impl DeviceDiscovery for PSCIDiscovery {
     fn am_i_this(&self, node: DeviceNode) -> Option<DeviceType> {
-        if let DeviceNode::DTB(fdt_node) = node {
-            kprintln!("TESTING NODE: {}", fdt_node.name);
-        }
         if let DeviceNode::DTB(fdt_node) = node
             && let Some(c) = fdt_node.compatible()
-            && c.all().any(|s| matches!(s, "arm,psci" | "arm,psci-0.2" | "arm,psci-1.0"))
+            && c.all()
+                .any(|s| matches!(s, "arm,psci" | "arm,psci-0.2" | "arm,psci-1.0"))
         {
-            kprintln!("FOUND MATCH");
             let method = fdt_node.property("method")?.as_str()?;
             let psci_method = match method {
                 "hvc" => PSCIMethod::HVC,
@@ -65,9 +71,11 @@ impl DeviceDiscovery for PSCIDiscovery {
             let migrate = u32::from_be_bytes(fdt_node.property("migrate")?.value.try_into().ok()?);
             let cpu_on = u32::from_be_bytes(fdt_node.property("cpu_on")?.value.try_into().ok()?);
             let cpu_off = u32::from_be_bytes(fdt_node.property("cpu_off")?.value.try_into().ok()?);
-            let cpu_suspend = u32::from_be_bytes(fdt_node.property("cpu_suspend")?.value.try_into().ok()?);
+            let cpu_suspend =
+                u32::from_be_bytes(fdt_node.property("cpu_suspend")?.value.try_into().ok()?);
 
-            PSCI_DEVICE.call_once(|| PSCIDevice::new(migrate, cpu_on, cpu_off, cpu_suspend, psci_method));
+            PSCI_DEVICE
+                .call_once(|| PSCIDevice::new(migrate, cpu_on, cpu_off, cpu_suspend, psci_method));
 
             return Some(DeviceType::Special);
         }
