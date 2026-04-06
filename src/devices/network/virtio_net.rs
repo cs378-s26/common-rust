555bbb19d9cd676ea87025445aed5455b99c76d6
Helen Gordon <gordons@wireless-10-145-20-122.public.utexas.edu>
2026-04-06 15:18:03 -0500

    devices: update virtio net driver to virtio-drivers 0.13.0 API

--- src/devices/network/virtio_net.rs
+++ src/devices/network/virtio_net.rs
@@ -5,20 +5,20 @@ use crate::devices::Device;
 use crate::physical_memory::{HHDM_REQUEST, alloc_frames, frame_dealloc};
 use crate::virtual_memory::{PagingOptions, VirtualMemoryAllocation};
 use core::ptr::NonNull;
-use virtio_drivers::device::net::VirtIONet;
+use virtio_drivers::device::net::{TxBuffer, VirtIONet};
 use virtio_drivers::transport::Transport;
 use virtio_drivers::{BufferDirection, Hal, PhysAddr};
 
 /// VirtIONetDriver wraps the virtio-drivers VirtIONet device and ties it to our kernel's HAL
 /// constructed from a transport (MMIO) and used by the device framework to send and receive packets
-pub struct VirtIONetDriver<H: Hal, T: Transport> {
-    net: VirtIONet<H, T>,
+pub struct VirtIONetDriver<H: Hal, T: Transport, const QUEUE_SIZE: usize> {
+    net: VirtIONet<H, T, QUEUE_SIZE>,
 }
 
-impl<T: Transport> VirtIONetDriver<VirtioNetHal, T> {
+impl<T: Transport> VirtIONetDriver<VirtioNetHal, T, 16> {
     pub fn new(transport: T) -> Self {
         Self {
-            net: VirtIONet::<VirtioNetHal, T>::new(transport)
+            net: VirtIONet::<VirtioNetHal, T, 16>::new(transport, 1536)
                 .expect("failed to initialize virtio net device"),
         }
     }
@@ -122,21 +122,30 @@ unsafe impl Hal for VirtioNetHal {
     }
 }
 
-impl<T: Transport> NetworkDevice for VirtIONetDriver<VirtioNetHal, T> {
+impl<T: Transport> NetworkDevice for VirtIONetDriver<VirtioNetHal, T, 16> {
     fn name(&self) -> &str {
         "virtio_net"
     }
 
     fn send_packet(&mut self, packet: &[u8]) -> Result<(), NetworkError> {
-        self.net.send(packet).map_err(|_| NetworkError::SendError)
+        self.net
+            .send(TxBuffer::from(packet))
+            .map_err(|_| NetworkError::SendError)
     }
 
     fn receive_packet(&mut self, buffer: &mut [u8]) -> Result<usize, NetworkError> {
-        self.net.receive(buffer).map_err(|_| NetworkError::ReceiveError)
+        let rx_buf = self.net.receive().map_err(|_| NetworkError::ReceiveError)?;
+        let packet = rx_buf.packet();
+        let len = packet.len().min(buffer.len());
+        buffer[..len].copy_from_slice(&packet[..len]);
+        self.net
+            .recycle_rx_buffer(rx_buf)
+            .map_err(|_| NetworkError::ReceiveError)?;
+        Ok(len)
     }
 }
 
-impl<T: Transport> Device for VirtIONetDriver<VirtioNetHal, T> {
+impl<T: Transport> Device for VirtIONetDriver<VirtioNetHal, T, 16> {
     #[allow(unused_variables)]
     fn ioctl(&self, request: u64, arg1: u64, arg2: u64) -> u64 {
         0 // stub 0 = success required by Device supertrait
