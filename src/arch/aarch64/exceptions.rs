11a7e3df05fb984c88c0c3807a569e15067792d4
Lucas Hollinger <88513496+lthollinger@users.noreply.github.com>
2026-04-14 15:23:25 -0500

    syscall scaffolding so we can all start implementing them

--- src/arch/aarch64/exceptions.rs
+++ src/arch/aarch64/exceptions.rs
@@ -10,6 +10,7 @@ use crate::{
     memory::virtual_memory::PageFaultConditions,
     mp::CORE_ID,
     print::kprintln,
+    syscall::syscall_handler,
     thread::{block_to_idle, preempt_to_idle, this_thread},
 };
 
@@ -57,7 +58,10 @@ fn default_exception_handler(exc: &mut ExceptionContext) {
         exc.elr_el1 += 4;
         exc.spsr_el1 &= !(1 << 7); // clear IRQ mask.
         // TODO write an architecture agnostic system call trap_frame that ExceptionContext implements so system calls can be passed this and just work
-        // system_call_handler(exc);
+
+
+        // uncomment this once the other prs are in
+        // syscall_handler(exc);
 
         return;
     } else if exception_class == INSTRUCTION_ABORT || exception_class == INSTRUCTION_ABORT_LOWER {
