0d5a4f69ae2d45e0daa1fc67285d285e356b3655
antd6@protonmail.com <antd6@protonmail.com>
2026-04-24 17:03:44 -0500

    Enable PS2 starutp sequence

--- src/devices/discovery/acpi.rs
+++ src/devices/discovery/acpi.rs
@@ -497,9 +497,10 @@ pub fn parse_acpi() -> Option<Vec<DeviceType>> {
     matched_devices.extend(init_pcie(mcfg.clone()));
 
     if let Some(fadt) = fadt_ {
+        kprintln!("Found FADT!");
         //source: https://elixir.bootlin.com/linux/v7.0.1/source/include/acpi/actbl.h#L261
         let ps2_enabled = fadt.flags & (1 << 1) != 0;
-        //kprintln!("PS2 enabled? {}", ps2_enabled);
+        kprintln!("PS2 enabled? {}", ps2_enabled);
         //if ps2_enabled {
             crate::devices::char::ps2_kb_m::init_ps2().ok()?;
             //matched_devices.push(DeviceType::Char(crate::devices::char::ps2_kb_m::init_ps2));
